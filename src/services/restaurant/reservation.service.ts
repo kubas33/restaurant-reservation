5d60e263f59d8df7fa23c5f8ae7a6226042cbc9c
kubas33 <44829224+kubas33@users.noreply.github.com>
2024-07-22 22:21:21 +0200

    Dodanie sprawdzania, czy termin rezerwacji zajęty

--- src/services/restaurant/reservation.service.ts
+++ src/services/restaurant/reservation.service.ts
@@ -1,28 +1,55 @@
-import { AppDataSource } from "configs/app-data-source";
-import { Reservation } from "entities/restaurant/reservation.entity";
-import { StringError } from "errors/string.error";
-import { IDeleteById } from "interfaces/common.interface";
-import { ICreateReservation, IUpdateReservation } from "interfaces/reservation.interface";
-import ApiUtility from "utilities/api.utility";
-import DateTimeUtility from "utilities/date-time.utility";
+import { AppDataSource } from 'configs/app-data-source';
+import { Reservation } from 'entities/restaurant/reservation.entity';
+import { StringError } from 'errors/string.error';
+import { IDeleteById } from 'interfaces/common.interface';
+import { ICreateReservation, IUpdateReservation } from 'interfaces/reservation.interface';
+import ApiUtility from 'utilities/api.utility';
+import DateTimeUtility from 'utilities/date-time.utility';
+import { Table } from 'entities/restaurant/table.entity';
+import { User } from 'entities/user/user.entity';
+import { Between, LessThan, MoreThan } from 'typeorm';
 
 const where = { isDeleted: false };
 
 const create = async (params: ICreateReservation) => {
-    const table = await AppDataSource.getRepository(Reservation).findOne({
-        where: { id: params.tableId }
+    const table = await AppDataSource.getRepository(Table).findOne({
+        where: { id: params.tableId },
+        relations: ['restaurant']
     })
 
     if (!table) {
-        throw new StringError('Table not found');
+        throw new StringError(`Table with id ${params.tableId} not found`);
     }
 
-    const user = await AppDataSource.getRepository(Reservation).findOne({
+    const user = await AppDataSource.getRepository(User).findOne({
         where: { id: params.userId }
     })
 
     if (!user) {
         throw new StringError('User not found');
+    }
+
+    // Sprawdzenie kolizji rezerwacji
+    const overlappingReservations = await AppDataSource.getRepository(Reservation).find({
+        where: [
+            {
+                table: table,
+                startTime: Between(params.startTime, params.endTime)
+            },
+            {
+                table: table,
+                endTime: Between(params.startTime, params.endTime)
+            },
+            {
+                table: table,
+                startTime: LessThan(params.startTime),
+                endTime: MoreThan(params.endTime)
+            }
+        ]
+    });
+
+    if (overlappingReservations.length > 0) {
+        throw new StringError('The table is already reserved for the given time range.');
     }
 
     const reservation = AppDataSource.getRepository(Reservation).create({
